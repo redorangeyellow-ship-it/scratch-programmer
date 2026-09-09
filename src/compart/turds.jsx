6f6185d30e91e779f6342109f0f707b4c8762d6a
redorangeyellow-ship-it <darkred95@gmail.com>
2026-09-09 11:33:10 -0400

    Add turds.jsx file with initial content
    Initial content for the turds.jsx file added.

---
+++ src/compart/turds.jsx
@@ -0,0 +1,55 @@
+
+keyman-book
+Repository navigation
+Code
+Issues
+Pull requests
+Agents
+Actions
+Projects
+Wiki
+Pull requests: redorangeyellow-ship-it/keyman-book
+
+ Labels 9
+ Milestones 0
+ 
+Welcome to pull requests!
+Pull requests help you collaborate on code with other people. As pull requests are created, they’ll appear here in a searchable and filterable list. To get started, you should create a pull request.
+
+ ProTip! Notify someone on an issue with a mention, like: @redorangeyellow-ship-it.
+keyman-book
+Repository navigation
+Code
+Issues
+Pull requests
+Agents
+Actions
+Projects
+Wiki
+Pull requests: redorangeyellow-ship-it/keyman-book
+
+ Labels 9
+ Milestones 0
+ 
+Welcome to pull requests!
+Pull requests help you collaborate on code with other people. As pull requests are created, they’ll appear here in a searchable and filterable list. To get started, you should create a pull request.
+
+ ProTip! Notify someone on an issue with a mention, like: @redorangeyellow-ship-it.
+keyman-book
+Repository navigation
+Code
+Issues
+Pull requests
+Agents
+Actions
+Projects
+Wiki
+Pull requests: redorangeyellow-ship-it/keyman-book
+Add turds.jsx file with initial content
+ Labels 9
+ Milestones 0
+ 
+Welcome to pull requests!
+Pull requests help you collaborate on code with other people. As pull requests are created, they’ll appear here in a searchable and filterable list. To get started, you should create a pull request.
+
+ ProTip! Notify someone on an issue with a mention, like: @redorangeyellow-ship-it.
